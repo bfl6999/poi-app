3e6a8b2c9b01246cf464643f954940fc4a5a2e09
Brian Flores Lopez <bfl699@uma.es>
2025-06-18 21:54:19 +0200

    Update test search in home

--- frontend/cypress/e2e/search.cy.ts
+++ frontend/cypress/e2e/search.cy.ts
@@ -1,10 +1,13 @@
 describe('Search POIs', () => {
   it('should search POIs by name', () => {
     cy.visit('/home');
+
+    // 1. Abrir el ion-select
     cy.get('ion-select').click();
-    // 2. Esperar el modal y hacer click en "Nombre"
+
+    // 2. Seleccionar "Nombre" desde ion-alert
     cy.get('ion-alert')
-      .should('be.visible')
+      .should('exist')
       .find('button.alert-radio-button')
       .contains('Nombre')
       .click({ force: true });
@@ -15,15 +18,19 @@ describe('Search POIs', () => {
       .contains('OK')
       .click({ force: true });
 
-    // 4. Esperar input por nombre y buscar
-    cy.get('ion-input[formControlName="name"] input')
+    // 4. Esperar que el input esté en el DOM
+    cy.get('ion-input[formControlName="name"] input', { timeout: 6000 })
       .should('exist')
-      .type('Parque');
+      .should('be.visible');
 
+    // 5. Escribir texto
+    cy.get('ion-input[formControlName="name"] input')
+      .type('Parque', { delay: 100 });
+
+    // 6. Enviar formulario
     cy.get('form').submit();
+
+    // 7. Comprobar resultados
     cy.contains('Parque').should('exist');
   });
-
-  // Pendiente subir otros filtros 
-
 });
