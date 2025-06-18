cbd92808184e1b1c1606ae1e015a36330e87d020
Brian Flores Lopez <bfl699@uma.es>
2025-06-18 22:47:39 +0200

    Update cypress run

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    video: false,
+    video: true,
     screenshotOnRunFailure: true
   }
 });
